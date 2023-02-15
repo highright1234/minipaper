242314a2856c7e30a8fdeda02d6614b514eec111
하이라이트 <77767930+highright1234@users.noreply.github.com>
2023-02-16 01:15:47 +0900

    :white_check_mark: papi

--- minipaper-bukkit-core/src/main/kotlin/io/github/highright1234/minipaper/Papi.kt
+++ minipaper-bukkit-core/src/main/kotlin/io/github/highright1234/minipaper/Papi.kt
@@ -1,5 +1,6 @@
 package io.github.highright1234.minipaper
 
+import io.github.highright1234.minipaper.util.processor
 import io.github.highright1234.shotokonoko.papi.papi
 import kotlinx.coroutines.runBlocking
 
@@ -7,16 +8,32 @@ object Papi {
     fun register() {
         papi {
             expansion("minipaper") {
+                then("playing_game") {
+                    executes {
+                        player?.processor?.gameInfo?.name
+                    }
+                }
                 argument("game") {
-                    argument("property") {
-                        argument("field") {
-                            executes {
-                                val gameName by arguments
-                                val property by arguments
-                                val field by arguments
-                                val game = MiniPaper.gameManager.games[gameName]!!
-                                val datastore = game.getProperty(property)
-                                runBlocking { datastore.get(field, String::class.java) }
+                    then("player_count") {
+                        executes {
+                            val gameName by arguments
+                            MiniPaper.gameManager.games[gameName]!!
+                                .processors
+                                .sumOf { it.onlinePlayers.size }
+                                .toString() // TODO bungee
+                        }
+                    }
+                    then("datastore") {
+                        argument("property") {
+                            argument("field") {
+                                executes {
+                                    val gameName by arguments
+                                    val property by arguments
+                                    val field by arguments
+                                    val game = MiniPaper.gameManager.games[gameName]!!
+                                    val datastore = game.getProperty(property)
+                                    runBlocking { datastore.get(field, String::class.java) }
+                                }
                             }
                         }
                     }
